f6361e4122ce7773e5b304d0818eefc5e84684e8
Livia L <llin24@students.cs.ubc.ca>
2024-08-24 23:35:38 -0700

    added album details

---
+++ albumdetails.js
@@ -0,0 +1,26 @@
+// album details: name, album cover image source, and tracks for each album
+const svtCarat = {
+    name: "17 CARAT",
+    albumSrc: "images/AdoreU.jpg",
+    tracks: ["Shining Diamond", "Adore U", "Ah Yeah", "Jam Jam", "20"]
+}
+
+const attacca = {
+    name: "Attacca",
+    albumSrc: "images/Attacca.png",
+    tracks: ["To you", "Rock with you", "Crush", "Imperfect love", "I can't run away", "2 MINUS 1 (Bonus Track)"]
+}
+
+const faceTheSun = {
+    name: "Face the Sun",
+    albumSrc: "images/FaceTheSun.png",
+    tracks: ["Darl+ing", "HOT", "DON QUIXOTE", "March", "Domino", "Shadow", "'bout you", "IF you leave me", "Ash"]
+} 
+
+const svtHeaven = {
+    name: "SEVENTEENTH HEAVEN",
+    albumSrc: "images/GodOfMusic.jpg",
+    tracks: ["SOS", "God of Music", "Diamond Days", "Back 2 Back", "Monster", "Yawn", "Headliner"]
+}
+
+export const albums = [svtCarat, attacca, faceTheSun, svtHeaven]
